f19a1019350f4c85b0a6e2ef7a5a74509259e6d8
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-11-12 11:02:36 +0530

    Update JwtService.java

--- security/src/main/java/com/example/security/config/JwtService.java
+++ security/src/main/java/com/example/security/config/JwtService.java
@@ -1,10 +1,29 @@
 package com.example.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "Yr2ALglWPp3T7b2h1ZpEVimr01cZeLBOwbVvoOOKezQDIsbUK6P5wxbjRwkJ0UNd\n";
     public String extractUsername(String jwt) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
         return null;
     }
 }
